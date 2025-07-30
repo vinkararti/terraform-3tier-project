3815e6db6dcdfd4d4052a73b1bad8f9006c8827b
Reyaz Shaik <45187103+ReyazShaik@users.noreply.github.com>
2025-07-30 23:23:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -191,7 +191,7 @@ resource "aws_db_instance" "default" {
   allocated_storage    = 10
   db_name              = "mydb"
   engine               = "mysql"
-  engine_version       = "8.0.35"
+  engine_version       = "8.0.41"
   instance_class       = "db.t3.micro"
   username             = "admin"
   password             = "Root123456"
